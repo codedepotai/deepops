cf7eae7bc2e101d9f01465e759f48dd47cb37858
Mario Drumond <mario.drumond@codedepot.ch>
2024-12-04 18:19:09 +0100

    Fixes ai admin user configuration.

--- scripts/ai/create_ai_user.sh
+++ scripts/ai/create_ai_user.sh
@@ -9,27 +9,30 @@ ADMIN_CSR=admin-user.csr
 ADMIN_CRT=admin-user.crt
 source ../scripts/ai/ai_vars.sh
 
+
+# Update certificate with CODEDEPOT_API_HOST_IP
+sudo rm /etc/kubernetes/pki/apiserver.crt
+sudo rm /etc/kubernetes/pki/apiserver.key
+
+sudo kubeadm init phase certs apiserver --apiserver-cert-extra-sans $CODEDEPOT_CLUSTER_HOST
+
+# Creates cubeconfig for admin user -> WHAT DOES THIS DO?
 # Create user key and certificate
 openssl genrsa -out $ADMIN_KEY 2048
 
 openssl req -new -key $ADMIN_KEY -out $ADMIN_CSR -subj "/CN=admin-user/O=system:masters"
 
-openssl x509 -req -in $ADMIN_CSR \
+sudo openssl x509 -req -in $ADMIN_CSR \
     -CA /etc/kubernetes/pki/ca.crt \
     -CAkey /etc/kubernetes/pki/ca.key \
     -CAcreateserial \
     -out $ADMIN_CRT \
     -days 365
 
-# Update certificate with CODEDEPOT_API_HOST_IP
-sudo rm /etc/kubernetes/pki/apiserver.crt
-sudo rm /etc/kubernetes/pki/apiserver.key
+kubectl apply -f ../scripts/ai/config/admin-user.yml
 
-sudo kubeadm init phase certs apiserver --apiserver-cert-extra-sans $CODEDEPOT_API_HOST_IP
-
-# Creates cubeconfig for admin user -> WHAT DOES THIS DO?
 kubectl config set-cluster $CODEDEPOT_CLUSTER_NAME \
-    --server=https://$CODEDEPOT_API_HOST_IP:6443 \
+    --server=https://$CODEDEPOT_CLUSTER_HOST:6443 \
     --certificate-authority=/etc/kubernetes/pki/ca.crt \
     --embed-certs=true \
     --kubeconfig=$ADMIN_CONFIG
@@ -37,11 +40,15 @@ kubectl config set-cluster $CODEDEPOT_CLUSTER_NAME \
 kubectl config set-credentials admin-user \
     --client-certificate=$ADMIN_CRT \
     --client-key=$ADMIN_KEY \
+    --embed-certs=true \
     --kubeconfig=$ADMIN_CONFIG
 
 kubectl config set-context admin-user-context \
     --cluster=$CODEDEPOT_CLUSTER_NAME \
     --user=admin-user \
+    --kubeconfig=$ADMIN_CONFIG
+
+kubectl config use-context admin-user-context \
     --kubeconfig=$ADMIN_CONFIG
 
 cp $ADMIN_CONFIG $HOME/.kube/config.admin
